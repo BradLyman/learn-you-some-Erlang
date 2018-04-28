22f035bb6dc5cec4510bf281eca815ccef050b61
BradLyman <lyman.brad3211@gmail.com>
2018-04-28 13:53:37 -0700

    Write Doc As Markdown

--- weave.sh
+++ weave.sh
@@ -3,9 +3,11 @@
 IFS='~'
 FORMAT_CMD="sed -E -e 's/^/    /g' -e 's/^    %%% ?//g' -i {}"
 APPLY_CMD="find ./doc -name '*' -type f -exec $FORMAT_CMD \;"
+RENAME_CMD="find ./doc -name '*' -type f -exec mv {} {}.md \;"
 
 rm -r doc/*
 cp --parents -r src/* doc
 echo $APPLY_CMD
 eval $APPLY_CMD
+eval $RENAME_CMD
 
